d44c19615b36dbe4ca2944404fe3f5a8f6210316
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-09-11 12:10:00 +0100

    Setup quelix app using typescript nextjs and tailwind

---
+++ app/(Dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+
+export default function Dashboard() {
+  return (
+    <Button variant="default">Hello</Button>
+  )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Quellix AI app',
+  description: 'AI platform specifically designed to generate Image, video ,code , and text resume based on user prompt',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
