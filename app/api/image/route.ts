78ba6ddb388cf3cf6eb9b308e38c9d10d4c54dfe
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-09-18 16:05:12 +0100

    connect to the image api

---
+++ app/api/image/route.ts
@@ -0,0 +1,51 @@
+import { auth } from "@clerk/nextjs";
+import {Configuration} from "openai";
+import {OpenAIApi}  from "openai";
+import { NextResponse } from "next/server";
+
+
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
+export async function POST(
+  req: Request
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt, amount='1', resolution='512x512'  } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!amount) {
+      return new NextResponse("Amount is required.", { status: 500 });
+    }
+
+    if (!resolution) {
+      return new NextResponse("Resolution is required", { status: 400 });
+    }
+
+    const response = await openai.createImage({
+      prompt,
+      n: parseInt(amount,10),
+      size: resolution
+     
+    });
+
+  // Check if the response structure is as expected
+  if (response.data && response.data.data) {
+    return NextResponse.json(response.data.data);
+  } else {
+    return new NextResponse("Huh this is the bug ", { status: 500 });
+  }
+} catch (error) {
+  console.log('[IMAGE_ERROR]', error);
+  return new NextResponse("Internal Error", { status: 500 });
+}
+};
